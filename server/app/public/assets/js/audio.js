e70e57210c787831749d8e0698dedb3c05f48c03
Sam Decrock <sam.decrock@gmail.com>
2014-03-26 11:05:20 +0100

    unused code

--- server/app/public/assets/js/audio.js
+++ server/app/public/assets/js/audio.js
@@ -50,28 +50,10 @@ function updateAudioData () {
         // primaryNote
 }
 
-function draw(){
-    if(audio.paused) return;
-    analyser.getFloatFrequencyData(spectrum);
-    var guess = guessNote(spectrum),
-        hue = Math.round(guess.primaryNote/12*255),
-        alpha = guess.totalEnergy*100;
-    ctx.strokeStyle = 'hsla(' + hue + ', 50%, 10%, ' + alpha + ')';
-    ctx.lineWidth = 4;
-    ctx.beginPath();
-    for(var i = 0; i < spectrum.length; i++) {
-        ctx.lineTo(i/spectrum.length*c.width, 400+spectrum[i]*2);
-    }
-    //ctx.fillRect(0, 0, c.width, c.height);
-    ctx.globalCompositeOperation = 'lighter';
-    ctx.stroke();
-    ctx.globalCompositeOperation = 'source-over';
-    ctx.drawImage(c, -4, -1, c.width+8, c.height+8);
-}
 
 
 var songs = [
-        "/assets/data/nero.mp3"
+        "/assets/data/cdd.mp3"
 ];
 
 // function initSocket(){
