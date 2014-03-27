3334dbf328b20fb208218d8fd84c28e4cbbbc144
Robby Wauters <robbywauters@gmail.com>
2014-03-27 11:54:52 +0100

    port 3003 and iphone-iframe height bug

--- server/app/app.js
+++ server/app/app.js
@@ -24,7 +24,7 @@ var app = express();
 var lastCommittedImage = [];
 
 app.configure(function(){
-	app.set('port', process.env.PORT || 3000);
+	app.set('port', process.env.PORT || 3003);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 	app.use(express.favicon());
